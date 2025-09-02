570c720f4ae8ff8f5acc91d5eeb0c4b66690ce23
ewnd9 <ewndnine@gmail.com>
2025-09-02 11:45:18 +0300

    chore: allow to disable workers

--- packages/web/src/env.js
+++ packages/web/src/env.js
@@ -10,6 +10,15 @@ export const env = createEnv({
     AWS_BUCKET: z.string(),
     AWS_REGION: z.string(),
     AWS_ENDPOINT_URL: z.string().url(),
+    WORKERS_ENABLED: z
+      .preprocess((val) => {
+        if (typeof val === 'string') {
+          if (val.toLowerCase() === 'true') return true;
+          if (val.toLowerCase() === 'false') return false;
+        }
+        return val;
+      }, z.boolean())
+      .default(true),
     SOCKS5_PROXY: z.string().optional(),
   },
   client: {
@@ -27,6 +36,7 @@ export const env = createEnv({
     AWS_BUCKET: process.env.AWS_BUCKET,
     AWS_REGION: process.env.AWS_REGION,
     AWS_ENDPOINT_URL: process.env.AWS_ENDPOINT_URL,
+    WORKERS_ENABLED: process.env.WORKERS_ENABLED,
     SOCKS5_PROXY: process.env.SOCKS5_PROXY,
     NEXT_PUBLIC_AWS_BUCKET_PROXY_URL: process.env.NEXT_PUBLIC_AWS_BUCKET_PROXY_URL,
   },
--- packages/web/src/instrumentation.ts
+++ packages/web/src/instrumentation.ts
@@ -2,15 +2,20 @@ import 'reflect-metadata';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { env } = await import('./env');
     const { container } = await import('./server/di/container');
     const { WorkersService } = await import('./server/queue/workers');
     const workersService = container.resolve(WorkersService);
 
-    try {
-      await workersService.startWorkers();
-      console.log('Queue workers started via instrumentation');
-    } catch (error) {
-      console.error('Failed to start queue workers via instrumentation:', error);
+    if (env.WORKERS_ENABLED) {
+      try {
+        await workersService.startWorkers();
+        console.log('Queue workers started via instrumentation');
+      } catch (error) {
+        console.error('Failed to start queue workers via instrumentation:', error);
+      }
+    } else {
+      console.log('Queue workers disabled via env');
     }
   }
 }
